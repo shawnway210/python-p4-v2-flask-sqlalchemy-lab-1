13f0e640479da24155ca45d534ec385ec6d0220b
shawnconway <shawnway210@gmail.com>
2023-10-26 11:03:42 -0400

    flask lab 1

--- server/app.py
+++ server/app.py
@@ -21,6 +21,48 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        body = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        status = 200
+
+        return make_response(body, status)
+    else:
+        body = {
+            'message': f'Earthquake {id} not found.'
+        }
+        status = 404
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    count = len(earthquakes)
+    quakes_data =[]
+    for earthquake in earthquakes:
+        quake_data = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        quakes_data.append(quake_data)
+    response = {
+        'count': count,
+        'quakes': quakes_data
+    }
+    status = 200
+    
+    return make_response(response, status)
+
+
 
 
 if __name__ == '__main__':
